5d61de482da600559a9a4743b643b3c160afc5a4
Juan Navarro <bertacaro1996@gmail.com>
2018-05-17 09:26:20 -0600

    add, ActionReceiver as Java class to handle de Globak event

---
+++ app/src/main/java/ActionReceiver.java
@@ -0,0 +1,11 @@
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class ActionReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i("SendSMS", "SMS recibido");
+    }
+}
