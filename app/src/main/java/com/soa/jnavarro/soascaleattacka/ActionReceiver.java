b5738bb85fafbeb71a4c2a372657f3a8d7d1040d
Juan <jnavcamacho@gmail.com>
2018-05-18 15:14:36 -0600

    doc of ActionReceiver Class

--- app/src/main/java/com/soa/jnavarro/soascaleattacka/ActionReceiver.java
+++ app/src/main/java/com/soa/jnavarro/soascaleattacka/ActionReceiver.java
@@ -5,11 +5,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+/**
+ * Class to make operations when an Intent is sent.
+ */
 public class ActionReceiver extends BroadcastReceiver {
     @Override
+    /** Overwritten method that runs when the intent is catch */
     public void onReceive(Context context, Intent intent) {
         Log.i("SendSMS", "SMS recibido");
-
     }
-
 }
